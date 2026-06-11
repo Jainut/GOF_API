25ee8c686782e0ca1d941c9f63a2b823d2ae5d48
Rique <henriquegoncm@gmail.com>
2026-06-11 11:34:10 -0300

    Fix: configuring websocket cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server,{
 
     cors:{
-        origin:"[https://rastreabilidadegof.vercel.app, http://localhost:5173/]",
+        origin:['https://rastreabilidadegof.vercel.app', 'http://localhost:5173/'],
         methods: ["GET", "POST"],
         credentials:true
     }
