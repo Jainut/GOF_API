1e081521fb2f00b5c28224ebcd2169ccd49cdfcc
Rique <henriquegoncm@gmail.com>
2026-06-11 11:25:58 -0300

    fixing some bugs

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server,{
 
     cors:{
-        origin:["http://localhost:5173", "https://rastreabilidadegof.vercel.app/"],
+        origin:["http://localhost:5173", "https://rastreabilidadegof.vercel.app"],
 
         credentials:true
     }
